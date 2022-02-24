bf44b0155849563a66e44c3a67c8ba13d97ee109
topfreelancer828 <shywaewajane@gmail.com>
2022-02-25 04:38:54 +0900

    style-order-fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/front/js/sweetalert/sweetalert.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/front/css/styles.css')}}">
     @if(isset($page_sub_title) && ($page_sub_title == 'Profile'))
     <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
     @endif
+    <link rel="stylesheet" href="{{ asset('assets/front/css/styles.css')}}">
     <title> @if(isset($page_title) ) {{ __($page_title) }} @else {{__('MPL - Marketing and Production Limited')}} @endif</title>
     </head>
 <body>
